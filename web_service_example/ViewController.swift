36028cca11dc156d7851779df456a7c78bc7e64a
CEM TASKIN <cemtaskin82@gmail.com>
2024-06-03 14:43:27 +0300

    Initial Commit

---
+++ web_service_example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  web_service_example
+//
+//  Created by Cem TAŞKIN on 3.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
